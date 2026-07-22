dea6d113871e17cbb0d1e12dc8148bb1471202e4
kongkiat <kongkiat.pid@thailife.com>
2026-07-22 11:22:46 +0700

    Refactor plugin.xml and update services and actions to streamline implementation; migrate startup activity to ProjectActivity.

--- src/main/kotlin/plugins/startup/LineNumberStartupActivity.kt
+++ src/main/kotlin/plugins/startup/LineNumberStartupActivity.kt
@@ -2,12 +2,12 @@ package plugins.startup
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 import plugins.service.LineNumberService
 import plugins.settings.LineNumberSettings
 
-class LineNumberStartupActivity : StartupActivity.DumbAware {
-    override fun runActivity(project: Project) {
+class LineNumberStartupActivity : ProjectActivity {
+    override suspend fun execute(project: Project) {
         val settings = LineNumberSettings.getInstance()
 
         val service = project.service<LineNumberService>()
